6c54cd7e2665f880e647981e89a0ad380fb9b96f
Vikash kumar <vtech076@gmail.com>
2025-09-19 20:38:45 +0530

    render configuration url

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,6 +1,6 @@
-export const USER_API_END_POINT='http://localhost:8000/api/v1/user'
+export const USER_API_END_POINT='https://jobhunt-official.onrender.com/api/v1/user'
 // src/constants.js3
-export const JOB_API_END_POINT='http://localhost:8000/api/v1/job'
-export const APPLICATION_API_END_POINT='http://localhost:8000/api/v1/application'
+export const JOB_API_END_POINT='https://jobhunt-official.onrender.com/api/v1/job'
+export const APPLICATION_API_END_POINT='https://jobhunt-official.onrender.com/api/v1/application'
 
-export const COMPANY_API_END_POINT='http://localhost:8000/api/v1/company'
+export const COMPANY_API_END_POINT='https://jobhunt-official.onrender.com/api/v1/company'
