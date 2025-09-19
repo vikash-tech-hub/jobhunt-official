36c0a29179443df2d0440765713d09f08dbd9728
Vikash kumar <vtech076@gmail.com>
2025-09-19 20:45:09 +0530

    cors fix

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
  
 app.use(cors({
-  origin: ["http://localhost:5173", "https://jobhunt-official.onrender.com"],
+  origin: ["http://localhost:5173", "https://jobhunt-official.vercel.app"],
   credentials: true
 }));
 
